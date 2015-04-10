49ca8de25c98700cb1df7ba30dba8bf7c5c38f49
Corey Richardson <corey@octayn.net>
2015-04-10 16:54:06 -0400

    Use crates.io, update example

--- examples/simple/src/main.rs
+++ examples/simple/src/main.rs
@@ -1,13 +1,14 @@
 extern crate glfw;
 extern crate nice_glfw;
 extern crate gl;
+extern crate libc;
 
-use std::c_str::CString;
+use std::io::Write;
 use glfw::Context;
 
 fn main() {
-    let glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
-    let (window, _) = nice_glfw::WindowBuilder::new(&glfw)
+    let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
+    let (mut window, _) = nice_glfw::WindowBuilder::new(&mut glfw)
         .try_modern_context_hints()
         .size(800, 600)
         .create().expect("Couldn't create window :(");
@@ -16,7 +17,7 @@ fn main() {
 
     gl::load_with(|p| glfw.get_proc_address(p));
 
-    let version = gl::GetString(gl::VERSION);
-    let cs = unsafe { CString::new(version as *const i8, false) };
-    println!("OpenGL Version: {}", cs);
+    let version = unsafe { gl::GetString(gl::VERSION) };
+    print!("OpenGL Version: ");
+    let _ = std::io::stdout().write(unsafe { std::slice::from_raw_parts(version, libc::strlen(version as *const _) as usize) });
 }
